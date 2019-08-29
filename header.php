44ca826bd4d564dd814ca462fe3fa5c7038428fe
hoangdxgch16435 <hoangdxgch16435@fpt.edu.vn>
2019-08-29 23:33:40 +0700

    Update header.php

--- header.php
+++ header.php
@@ -7,10 +7,10 @@ session_start();
         <link rel="stylesheet" href="css/menu_style.css?v=<?php echo time();?>" type="text/css"/>
         <link rel="stylesheet" href="css/style.css?v=<?php echo time();?>" type="text/css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <title>ATN Manager</title>
+        <title>H Store Manager</title>
     </head>
     <body>
-    <center><img src="images/ToyStore.jpg"</center>
+    <center><img src="toy.png"</center>
     <?php
     require_once './functions.php';
         include_once './menu_admin.php';
