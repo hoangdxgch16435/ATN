2bce0df9690f95c037b2af713d4b581c1704598f
hoangdxgch16435 <hoangdxgch16435@fpt.edu.vn>
2019-08-29 21:54:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>ATN Toy Store</title>
+        <title>H Toy Store</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <style>
