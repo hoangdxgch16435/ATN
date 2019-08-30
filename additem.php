034a906140ef09f3042521cdbe2074bef31ed2bb
hoangdxgch16435 <hoangdxgch16435@fpt.edu.vn>
2019-08-30 10:15:13 +0700

    Update additem.php

--- additem.php
+++ additem.php
@@ -30,7 +30,7 @@ if (isset($_POST['iid'], $_POST['iname'], $_POST['idescription'], $_POST['iprice
 
 ?>
 <br><br>
-<form action="additem.php" method="POST" >
+<form action="additem.php" method="post" >
     <fieldset class = "fitcontenter">
 
         <legend>Add Item</legend>
@@ -50,17 +50,7 @@ if (isset($_POST['iid'], $_POST['iname'], $_POST['idescription'], $_POST['iprice
         <input type="text" name="isize" maxlength="15"/><br>     
         Image:<br>
         <input type="text" name="iimage"/><br>
-        Catalogue:<br>
-        <select name="cid">
-            <?php
-            $query = "SELECT cid, cname FROM catalogue";
-            $batches = pdo->query($query);
-            while ($batch = pg_fetch_array($batches)) {
-                $cId = $batch['cid'];
-                $cName = $batch['cname'];
-                echo "<option value='$cId'>$cName</option>";
-            }
-            ?>
+
         </select><br><br>
         <input type="submit" value="Add" name="add"/>
     </fieldset>
