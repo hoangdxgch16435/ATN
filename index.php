5b9c9e0ec40652a17339491d609d66eb9b0939db
hoangdxgch16435 <hoangdxgch16435@fpt.edu.vn>
2019-09-01 15:20:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ $result = queryMysql($query);
                         <center><li><a href="./header.php">Admin</a></li></center>
                         <li><a href="#Lego">Lego</a></li>
                         <li><a href="#Doll">Doll</a></li>
-                        <li><a href="#Clay">Clay</a></li>
+                        
 
                     </ul>
 
@@ -228,56 +228,9 @@ $result = queryMysql($query);
     }
 ?>
      
-                </div>       
-                    <div class="list w3-row">
-                    <div class=""id="Clay"><h2>Clay</h2>
-                    <?php
-     require_once './functions.php';
-    $query = "SELECT iid, iname, idescription, iprice, istatus, isize, iimage,cname FROM Item,Catalogue WHERE Item.cid=Catalogue.cid AND cName LIKE '%Clay%'  ORDER BY cname";
-     $result = queryMysql($query);
-     $result->setFetchMode(PDO::FETCH_ASSOC);
-     $result->execute();
-     $resultSet = $result->fetchAll();
-     
-     foreach ($resultSet as $row) {
-        $iId = $row['iid'];
-        $iName = $row['iname'];
-        $iDescription = $row['idescription'];
-        $iPrice = $row['iprice'];
-        $iStatus = $row['istatus'];
-        $iSize = $row['isize'];
-        $iImage = $row['iimage'];
-        
-        echo "<div class='sp w3-quarter w3-card w3-center ' ><div class='w3-orange w3-padding-large'>$iStatus</div><div ><img onclick=\"document.getElementById('$iName').style.display='block'\" id='testimg' src='./images/". $iImage . "' width='100%'></div><div class='name'><h3>$iName</h3></div><h3>$iPrice$</h3></div>"
-                . "<!--SHOW MORE INFORMATION-->
-  <div id='$iName' class='w3-modal'>
-      <div class='w3-modal-content w3-animate-top w3-card-4'>
-        <div class='w3-container w3-purple w3-center w3-padding-20'> 
-          <span onclick=\"document.getElementById('$iName').style.display='none';\"
-         class='w3-button w3-orange w3-xlarge w3-display-topright'>×</span>
-          <h2>$iName</h2>
-        </div>
-        <div class='w3-container w3-row'>
-          <div class='w3-half'>
-              <img src='./images/". $iImage . "' width='100%'>
-          </div>
-          <div class='w3-half w3-left'>
-              <h3>Price: $iPrice$</h3>
-              <p>Descripton: $iDescription.</p>
-              <h4>Size: $iSize</h4>                           
-          </div>                                                    
-        </div>
-        <button class='w3-button w3-orange w3-section' onclick=\"document.getElementById('$iName').style.display='none';\">Close <i class='fa fa-remove'></i></button>
-      </div>
-    </div>";                                                                                       
-    }
-?>
-  
-     
-                </div>
-
-                        
-            </div>
+                </div>  
+                </div>     
+                    
             <div class="footer">
               <div><center>Made by HoangPinLao</center></div>
             </div>
